135dd7c7fc74bbb98171da1161a041d560545543
Manish kumar <mk181610@gmail.com>
2025-07-18 12:57:26 +0530

    setup done laravel-react-inertia project

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [
-        react(),
         laravel({
             input: ["resources/js/app.jsx"],
             refresh: true,
         }),
+        react(),
     ],
-    server: {
-        host: "127.0.0.1",
-        port: 5173,
-    },
 });
