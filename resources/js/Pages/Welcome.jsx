46026b3f5bfe87862a9539f895badae10e7e3a7f
Manish kumar <mk181610@gmail.com>
2025-07-18 12:53:45 +0530

    react Inertia setup
    npm install --save-dev @vitejs/plugin-react
    
    composer require inertiajs/inertia-laravel
    
    php artisan inertia:middleware
    
    npm install @inertiajs/react

---
+++ resources/js/Pages/Welcome.jsx
@@ -0,0 +1,3 @@
+export default function Welcome() {
+    return <h1 className="text-3xl font-bold">Hello from Inertia + React!</h1>;
+}
---
+++ resources/views/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <meta charset="utf-8" />
+        <title>Agronil CRM</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        @viteReactRefresh
+        @vite('resources/js/app.jsx')
+        @inertiaHead
+    </head>
+
+    <body>
+        @inertia
+    </body>
+
+</html>
